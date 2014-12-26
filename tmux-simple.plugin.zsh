b9283cf4b166641ac7ea20f2d498e627f0ddb696
Tom Bloor <tbsliver@zith.local>
2014-12-26 13:13:58 +0000

    first commit

---
+++ tmux-simple.plugin.zsh
@@ -0,0 +1,15 @@
+# Simple plugin for Zsh for working with tmux
+
+# Provides colour exporting and working SSH socket manipulation
+
+source ~/.zsh-local-custom
+
+[ -z "$TMUX" ] && export TERM=xterm-256color
+
+SOCK="$HOME/.ssh/.ssh-agent-$USER-tmux"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+  rm -f $SOCK
+  ln -sf $SSH_AUTH_SOCK $SOCK
+  export SSH_AUTH_SOCK=$SOCK
+fi
