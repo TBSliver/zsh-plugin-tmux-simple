1e42219c840417873c88f4b65a9d3eeb18a88d50
Tom Bloor <tom.bloor@googlemail.com>
2016-11-30 10:17:13 +0000

    Add support for multi tmux sessions and and only change ssh sock when in a tmux

--- tmux-simple.plugin.zsh
+++ tmux-simple.plugin.zsh
@@ -4,10 +4,12 @@
 
 [ -z "$TMUX" ] && export TERM=xterm-256color
 
-SOCK="$HOME/.ssh/.ssh-agent-$USER-tmux"
-if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
-then
-  rm -f $SOCK
-  ln -sf $SSH_AUTH_SOCK $SOCK
-  export SSH_AUTH_SOCK=$SOCK
+if [ -n "$TMUX" ]; then
+  TMUX_SESSION=`tmux display-message -p '#S'`
+  SOCK="$HOME/.ssh/.ssh-agent-$USER-tmux-$TMUX_SESSION"
+  if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]; then
+    rm -f $SOCK
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+  fi
 fi
